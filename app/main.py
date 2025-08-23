d0cf218b2cf1b75b696ea5c2c9d91fb133005520
Hai Dang Nguyen <dunker1304@gmail.com>
2025-08-23 09:55:00 +0700

    run a program

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 import sys
 import os
+import subprocess
 
 list_buildin_cmd = ['exit', 'echo', 'type']
+
+def find_executable(command):
+    path_env = os.environ.get('PATH', '')
+    for d in path_env.split(':'):
+        if not d:
+            continue
+        candidate = os.path.join(d, command)
+        if os.path.isfile(candidate) and os.access(candidate, os.X_OK):
+            return candidate
+
+    return None                
+            
 
 def main():
     # Uncomment this block to pass the first stage
@@ -29,25 +42,38 @@ def main():
                 if query in list_buildin_cmd:
                     print(f"{query} is a shell builtin")
                 else:
-                    path_env = os.environ.get('PATH', '')
-                    found = False
-                    for d in path_env.split(':'):
-                        if not d:
-                            continue
-                        candidate = os.path.join(d, query)
-                        if os.path.isfile(candidate):
-                            if os.access(candidate, os.X_OK):
-                                print(f"{query} is {candidate}")
-                                found = True
-                                break
-                            else:
-                                continue
-
-                    if not found:
-                        print(f"{query}: not found")
+                    executable_path = find_executable(query)
+                    if executable_path:
+                        print(f"{query} is {executable_path}")
+                    else:
+                        print(f"{query} not found") 
 
             case _:
-                print(f"{command}: command not found")
+                executable_path = find_executable(command)
+                if executable_path:
+                    try:
+                        result = subprocess.run(
+                            [executable_path] + command_with_args[1:],
+                            capture_output=True,
+                            text=True,
+                            timeout=20
+                        )
+
+                        if result.stdout:
+                            print(result.stdout, end='')
+                        
+                        if result.stderr:
+                            print(result.stderr, end='')
+
+                        if result.returncode != 0:
+                            sys.exit(result.returncode)
+
+                    except subprocess.TimeoutExpired:
+                        print(f"{command}: command time out")
+                    except Exception as e:
+                        print(f"{command}: execution failed: {e}")
+                else:
+                    print(f"{command}: command not found")
         
 
 if __name__ == "__main__":
